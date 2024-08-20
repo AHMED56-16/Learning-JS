5c75fbbc41b59f7f2fdc8a4783c592d62511d479
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-08-20 14:24:57 +0500

    Slides Assigment updated

---
+++ slidesAssignments.js
@@ -0,0 +1,44 @@
+//Assignment # 1:
+/*
+    1.Create a "firstName" and "Last_Name" variables
+    2.Assign your values
+    3.Create "address" variable & assign "main street" valve to it
+    4.Re-assign adress to "first street" later
+    5.Log all values in the console
+*/
+
+let firstName;
+let Last_Name;
+
+firstName = 'Muhammad';
+Last_Name = 'Ahmed Raza';
+
+let address = 'main street';
+
+address = 'first street';
+
+console.log(firstName);
+
+console.log(Last_Name);
+
+console.log(address);
+
+// Assignment # 2:
+
+/*
+    1.Create "street" and "country" variables
+    2.Assign your values
+    3.Create "fullMailingAddress" variable and assign the result of "street + country"
+    4.Remember about the space
+    5.Log "fullMailingAddress" in the console
+*/
+
+let street;
+let country;
+
+street = '123 Main St';
+country="Pakistan";
+
+let fullMailingAddress = street + ' ' + country;
+
+console.log(fullMailingAddress);
