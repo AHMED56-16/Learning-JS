4411c2555df813983414539c4cea3b553fe6f3df
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-08-20 14:04:30 +0500

    Book assignment of chap 1 is completed

---
+++ bookAssignments.js
@@ -0,0 +1,40 @@
+//CHAPTER # 1-ALERTS:
+
+// Alerts are a simple way to display messages to the user. They can be used for various purposes, such as displaying error messages, confirming user actions, or providing feedback to the user.
+
+// Syntax:
+
+// alert(message);
+
+//Assignment # 1:Write a script to greet your website visitor uding JS alert box.
+
+alert("Hello, welcome to my website!");
+
+//Assignment # 2:Write a script to display following message on your webpage 
+// Error! Please enter a valid Password
+
+alert("Error! Please enter a valid Password");
+
+//Assignment # 3:Write a script to display following message on your webpage: (Hint : Use line break)
+//Welcome to JS land...
+//Happy Coding!
+
+alert("Welcome to JS land...\nHappy Coding!");
+
+//Assignment # 4:Write a script to display following message in seqvence:
+
+// 1.Welcome to JS land...
+// 2.Happy Coding!
+//Prevent this page from creating additional dialogues
+
+alert("Welcome to JS land...");
+alert("appy Coding!\nPrevent this page from creating additional dialogues");
+
+//Assignment # 5:Generate the following message through browser's developer console:
+
+//Hello.. I can run JS throgh my web browser's console
+
+// Assignment # 5: Generate the following message through browser's developer console:
+// Hello.. I can run JS through my web browser's console
+
+alert("Hello.. I can run JS through my web browser's console");
